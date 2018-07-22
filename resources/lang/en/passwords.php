733aea6c6d3d43f6519d28bf5cdb0781ee194997
Osama arafa <37809965+osamarafa1994@users.noreply.github.com>
2018-07-22 02:15:42 +0200

    Update passwords.php

--- resources/lang/en/passwords.php
+++ resources/lang/en/passwords.php
@@ -13,8 +13,8 @@ return [
     |
     */
 
-    'password' => 'Passwords must be at least six characters and match the confirmation.',
-    'reset' => 'Your password has been reset!',
+    'password' => 'كلمة المرور يجب ان تكون اكثر من 6 احرف ومتشابه.',
+    'reset' => 'رقمك السري تم ارساله!',
     'sent' => 'We have e-mailed your password reset link!',
     'token' => 'This password reset token is invalid.',
     'user' => "We can't find a user with that e-mail address.",
